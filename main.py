4531a8c9e4ccd3e0353842723bae5c0d016817ed
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-10-11 18:16:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,10 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 image = Image.open("MaIZeKFo_400x400.jpg")
 image = image.resize((50, 50))
 
+st.image(image, use_column_width=False, width=50)
+st.title("AtomPunks V0 Indexer")
 
-col1, col2= st.columns(2)
-
-with col1:
-   st.image(image, use_column_width=False, width=50)
-
-with col2:
-   st.title("AtomPunks V0 Indexer")
+   
 
     
     
