7bb24d90bb8845bb9974d4e2c80cc29232e308b3
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-10-11 17:10:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ st.title("AtomPunks V0 Indexer")
 # 加载 JSON 数据到 Pandas DataFrame
 atompunk_data_list_v0 = pd.read_json("atompunk_data_list_v0.json")
 
-# 使用HTML样式来使表格置中并最大化尺寸
+# 使用HTML样式来使表格置中并限制最大宽度
 st.markdown(
-    f'<div style="display: flex; justify-content: center;"><table style="width: 30%; max-width: 30%; text-align: center;">{atompunk_data_list_v0.to_html(classes="dataframe", index=False)}</table></div>',
+    f'<div style="display: flex; justify-content: center;"><table style="width: 80%; text-align: center;">{atompunk_data_list_v0.to_html(classes="dataframe", index=False)}</table></div>',
     unsafe_allow_html=True
 )
